5d2289efac152f74634f0e056ec5780527e2d95d
Jesus Ambro <68930496+jesusxambro@users.noreply.github.com>
2022-09-03 07:36:26 -0500

    Boiler plate spring initializer

---
+++ src/main/java/com/simplespringcrud/demo/SimpleSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com.simplespringcrud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplespringcrud/demo/SimpleSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplespringcrud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
